10319874c8a7bd2fdfe79aa9ae4daa7447e8235b
0x80 <netsecdeveloper@gmail.com>
2020-08-23 03:26:41 +0200

    check_all_unique_characters v2 implementation added
    new implementation use extra data structure

--- src/unique_characters.cpp
+++ src/unique_characters.cpp
@@ -1,6 +1,6 @@
 #include "unique_characters.h"
 
-auto check_all_unique_characters(const std::string &param)
+auto check_all_unique_charactersv1(const std::string &param)
 	-> bool
 	{
 	if (param.length() > 256)
@@ -10,6 +10,23 @@ auto check_all_unique_characters(const std::string &param)
 		{
 		if(param.find(ch, index++) != std::string::npos)
 			return false;
+		}
+	return true;	
+	}
+
+auto check_all_unique_charactersv2(const std::string &param) 
+	-> bool
+	{
+	if (param.length() > 256)
+		return false;
+	auto index = size_t{0};
+	bool ascii_set[256] = { false };
+	for(auto ch : param)
+		{
+		index = ch;
+		if(ascii_set[index])
+			return false;
+		ascii_set[index] = true;
 		}
 	return true;	
 	}
--- src/unique_characters.h
+++ src/unique_characters.h
@@ -1,5 +1,9 @@
 #include <string>
 
 //Implementation of algorithm to determine if a string has all unique characters. 
-//Note : you can not use additional data structures.
-auto check_all_unique_characters(const std::string &param) -> bool;
+//Note : not used additional data structures.
+auto check_all_unique_charactersv1(const std::string &param) -> bool;
+
+//Implementation of algorithm to determine if a string has all unique characters. 
+//Note : used additional data structures.
+auto check_all_unique_charactersv2(const std::string &param) -> bool;
