fb8d0343630e0ee9d3625c72d888f8048f514fb9
0x80 <netsecdeveloper@gmail.com>
2020-08-31 02:17:35 +0200

    reverse_string function added
    not use additional data structures

---
+++ src/reverse_string.cpp
@@ -0,0 +1,26 @@
+#include "reverse_string.h"
+
+//Implementation of reversing null-terminated string.
+//Note : not use additional data structures.
+auto reverse_string(char * const param) 
+	-> char*
+	{
+	auto start = param;
+	auto size  = size_t{0};
+	while(*start++ != '\0')
+		++size;
+	if(!size)
+		return param;
+	auto opcount = static_cast<size_t>(size/2);
+	auto ch      = char{0};
+	auto index   = size_t{0}; 
+	for(size_t iter = 0; iter < opcount; ++iter)
+		{
+		index 		 = size - iter - 1;
+		ch           = param[iter];
+		param[iter]  = param[index];
+		param[index] = ch;
+		}
+
+	return param;
+	}
---
+++ src/reverse_string.h
@@ -0,0 +1,3 @@
+//Implementation of reversing null-terminated string.
+//Note : you can not use additional data structures.
+auto reverse_string(char * const param) -> char*;
