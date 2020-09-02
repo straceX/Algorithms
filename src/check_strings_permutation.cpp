15b5ac0dc52b3a3bafc5edbed091fdb5b0c74094
0x80 <netsecdeveloper@gmail.com>
2020-09-02 13:08:37 +0200

    check_strings_permutationv2 function added
    second implementation of check_strings_permutation not use extra data structure. First implementation is updated.

--- src/check_strings_permutation.cpp
+++ src/check_strings_permutation.cpp
@@ -1,12 +1,14 @@
 #include "check_strings_permutation.h"
 
-auto check_strings_permutation(const std::string &lhs, const std::string &rhs) 
+#include <algorithm>
+
+auto check_strings_permutationv1(const std::string &lhs, const std::string &rhs) 
 	-> bool
 	{
-	if(lhs.length() < rhs.length())
-		return check_strings_permutation(rhs, lhs);
-	
-	int ascii_set[256] = {0};
+	if(lhs.length() != rhs.length())
+		return false;
+	constexpr const size_t ASCII_LENGTH{256};
+	int ascii_set[ASCII_LENGTH] = {0};
 	auto index =  size_t{0};
 	for(auto ch : lhs)
 		{ 
@@ -20,11 +22,29 @@ auto check_strings_permutation(const std::string &lhs, const std::string &rhs)
 		--ascii_set[index]; 
 		}
 
-	for(index = 0; index < 256; ++index)
+	for(index = 0; index < ASCII_LENGTH; ++index)
 		{
-		if(ascii_set[index] < 0)
+		if(ascii_set[index])
 			return false;
 		}
+	
+	return true;
+	}
+
+auto check_strings_permutationv2(std::string &lhs, std::string &rhs) 
+	-> bool
+	{
+	if(lhs.length() != rhs.length())
+		return false;
+
+	std::sort(std::begin(lhs), std::end(lhs));
+	std::sort(std::begin(rhs), std::end(rhs));
+
+	for(size_t index = 0; index < rhs.length(); ++index)
+		{
+		if(lhs[index] != rhs[index])
+			return false;
+		}	
 	
 	return true;
 	}
--- src/check_strings_permutation.h
+++ src/check_strings_permutation.h
@@ -1,4 +1,8 @@
 #include <string>
 
 //Implementation of checking two strings for one is a permutation of the other
-auto check_strings_permutation(const std::string &lhs, const std::string &rhs) -> bool;
+//Not: used extra data structure
+auto check_strings_permutationv1(const std::string &lhs, const std::string &rhs) -> bool;
+
+//Not: not used extra data structure
+auto check_strings_permutationv2(std::string &lhs, std::string &rhs) -> bool;
