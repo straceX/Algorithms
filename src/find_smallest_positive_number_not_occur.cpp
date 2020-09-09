f3df8695d7ec9372dee3c2dd2d00d62423d648fe
0x80 <netsecdeveloper@gmail.com>
2020-09-10 01:10:20 +0200

    find_smallest_positive_number_not_occur second version is added
    new version not change container order

--- src/find_smallest_positive_number_not_occur.cpp
+++ src/find_smallest_positive_number_not_occur.cpp
@@ -2,7 +2,7 @@
 
 #include <algorithm>
 
-auto find_smallest_positive_number_not_occur(std::vector<int> &param) 
+auto find_smallest_positive_number_not_occurv1(std::vector<int> &param) 
     -> int
     {
     std::sort(param.begin(), param.end());
@@ -20,6 +20,19 @@ auto find_smallest_positive_number_not_occur(std::vector<int> &param)
             continue;
             }
         return result;
+        }
+    return result;
+    }
+
+auto find_smallest_positive_number_not_occurv2(const std::vector<int> &param)
+    -> int
+    {
+    auto result = size_t{ 1 };
+    for (size_t index = 0; index < param.size(); ++index)
+        {
+        if (auto res = std::find(std::begin(param), std::cend(param), result); res == std::cend(param))
+            return result;
+        ++result;
         }
     return result;
     }
--- src/find_smallest_positive_number_not_occur.h
+++ src/find_smallest_positive_number_not_occur.h
@@ -4,4 +4,8 @@
 
 //find smallest positive integer that does not occur in container
 //Note: update container | complexity : O(N) or O(N * log(N)) 
-auto find_smallest_positive_number_not_occur(std::vector<int> &param) -> int;
+auto find_smallest_positive_number_not_occurv1(std::vector<int> &param) -> int;
+
+//find smallest positive integer that does not occur in container
+//Note: container is const | complexity :
+auto find_smallest_positive_number_not_occurv2(const std::vector<int> &param) -> int;
