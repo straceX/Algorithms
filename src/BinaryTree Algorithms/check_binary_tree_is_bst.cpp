b57b1aea69fe52431ecc89f0f50945587ef7abe2
0x80 <netsecdeveloper@gmail.com>
2020-09-15 23:01:44 +0200

    second version of checking binary tree is binary search tree function is added

--- src/BinaryTree Algorithms/check_binary_tree_is_bst.cpp
+++ src/BinaryTree Algorithms/check_binary_tree_is_bst.cpp
@@ -3,19 +3,33 @@
 #include "binary_tree.h"
 #include "check_binary_search_tree.h"
 
-auto check_binary_tree_is_bst(const BinaryTree::Node *root, int &pivotValue) 
+auto check_binary_tree_is_bstv1(const BinaryTree::Node *root, int &pivotValue) 
 	-> bool
 	{
 	if (!root) 
 		return true;
-	if (!check_binary_tree_is_bst(root->left, pivotValue))
+	if (!check_binary_tree_is_bstv1(root->left, pivotValue))
 		return false;
 	
 	if (root->data <= pivotValue)
 		return false;
 	pivotValue = root->data;
 
-	if (!check_binary_tree_is_bst(root->right, pivotValue))
+	if (!check_binary_tree_is_bstv1(root->right, pivotValue))
+		return false;
+
+	return true;
+	}
+
+auto check_binary_tree_is_bstv2(const BinaryTree::Node *root, int min = std::numeric_limits<int>::min(), int max = std::numeric_limits<int>::max()) 
+	-> bool
+	{
+	if (!root) 
+		return true;
+	if(!(root->data > min && root->data < max))
+		return false;
+
+	if( !(check_binary_tree_is_bstv2(root->left, min, root->data) && check_binary_tree_is_bstv2(root->right, root->data, max)) )
 		return false;
 
 	return true;
--- src/BinaryTree Algorithms/check_binary_tree_is_bst.h
+++ src/BinaryTree Algorithms/check_binary_tree_is_bst.h
@@ -1,5 +1,9 @@
 #pragma once
 
 struct BinaryTree::Node;
+
 //Implementation of checking binary tree is binary search tree
-auto check_binary_tree_is_bst(const BinaryTree::Node *root, int &pivotValue) -> bool;
+
+auto check_binary_tree_is_bstv1(const BinaryTree::Node *root, int &pivotValue) -> bool;
+
+auto check_binary_tree_is_bstv2(const BinaryTree::Node *root, int min, int max) -> bool;
