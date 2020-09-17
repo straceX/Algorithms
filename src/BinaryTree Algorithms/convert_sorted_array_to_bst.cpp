47b44542eeafc1dfe2448eb33386a0ae466c9ade
0x80 <netsecdeveloper@gmail.com>
2020-09-17 14:46:50 +0200

    sorted array to bst conversion function is added
    Implementation of converting sorted unique integer array elements to binary search tree data structure

---
+++ src/BinaryTree Algorithms/convert_sorted_array_to_bst.cpp
@@ -0,0 +1,16 @@
+#include "convert_sorted_array_to_bst.h"
+
+auto convert_sorted_array_to_bst(const int arry[], const size_t size) 
+	-> BinaryTree::Node*
+	{
+	if(!size)
+		return nullptr;
+	
+	auto mid = (size - 1) / 2;
+    auto root = new BinaryTree::Node;
+    
+    root->data  = arry[mid];
+    root->left  = convert_sorted_array_to_bst(arry, mid);
+    root->right = convert_sorted_array_to_bst(arry + mid + 1, size - mid - 1);    
+    return root;
+	}
---
+++ src/BinaryTree Algorithms/convert_sorted_array_to_bst.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "binary_tree.h"
+
+//Implementation of converting sorted unique integer array elements to binary search tree
+auto convert_sorted_array_to_bst(int arry[], size_t size) -> BinaryTree::Node*;
