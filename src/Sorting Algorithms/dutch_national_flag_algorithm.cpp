23fed6c73d289965784ddfe8352ee306d48387c7
0x80 <netsecdeveloper@gmail.com>
2020-09-18 15:32:10 +0200

    dutch national flag algorithm second implementation is added
    this implementation needs elements value are : 0, 1, 2

--- src/Sorting Algorithms/dutch_national_flag_algorithm.cpp
+++ src/Sorting Algorithms/dutch_national_flag_algorithm.cpp
@@ -2,7 +2,41 @@
 
 #include <algorithm>
 
-auto dutch_national_flag_algorithm(int arry[], const size_t size)
+auto dutch_national_flag_algorithmv1(int *arry, const size_t size)
+	-> void
+	{
+    auto cnt0 = size_t{0};
+    auto cnt1 = size_t{0};
+  
+    for (auto index = size_t{0}; index < size; ++index) 
+        { 
+        switch (arry[index])
+            { 
+            case 0: 
+                ++cnt0; 
+                break; 
+            case 1: 
+                ++cnt1; 
+                break; 
+            case 2:
+                break;
+            default:
+                return; 
+            } 
+        } 
+
+    for (auto index = size_t{0}; index < size; ++index) 
+        { 
+        if (index < cnt0)
+            arry[index] = 0; 
+        else if(index >= cnt0 && index < cnt0 + cnt1)
+            arry[index] = 1;
+        else
+            arry[index] = 2;
+        }
+	}
+
+auto dutch_national_flag_algorithmv2(int arry[], const size_t size)
 	-> void
 	{
 	auto sindex = size_t{0};
--- src/Sorting Algorithms/dutch_national_flag_algorithm.h
+++ src/Sorting Algorithms/dutch_national_flag_algorithm.h
@@ -1,2 +1,4 @@
 
-auto dutch_national_flag_algorithm(int arry[], const size_t size) -> void;
+auto dutch_national_flag_algorithmv1(int arry[], const size_t size) -> void;
+
+auto dutch_national_flag_algorithmv2(int arry[], const size_t size) -> void;
