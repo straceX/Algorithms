6469a5fdaa9aa0522cc5a973ed0ddc55198293de
0x80 <netsecdeveloper@gmail.com>
2020-09-23 14:50:20 +0200

    Fixed code review remarks

--- src/BinaryTree Algorithms/convert_sorted_array_to_bst.cpp
+++ src/BinaryTree Algorithms/convert_sorted_array_to_bst.cpp
@@ -7,10 +7,10 @@ auto convert_sorted_array_to_bst(const int arry[], const size_t size)
 		return nullptr;
 	
 	auto mid = (size - 1) / 2;
-    auto root = new BinaryTree::Node;
+	auto root = new BinaryTree::Node;
     
-    root->data  = arry[mid];
-    root->left  = convert_sorted_array_to_bst(arry, mid);
-    root->right = convert_sorted_array_to_bst(arry + mid + 1, size - mid - 1);    
-    return root;
+	root->data  = arry[mid];
+	root->left  = convert_sorted_array_to_bst(arry, mid);
+	root->right = convert_sorted_array_to_bst(arry + mid + 1, size - mid - 1);    
+	return root;
 	}
